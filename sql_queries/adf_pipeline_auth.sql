db7e4d659fa3cc2c6a3bae6d6f7a46d5e5399e2c
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2026-05-17 01:31:06 +0200

    adding files

--- sql_queries/adf_pipeline_auth.sql
+++ sql_queries/adf_pipeline_auth.sql
@@ -1,8 +1,8 @@
 -- This is needed to setup linked service in ADF pipeline successfully for dedicated SQL pool
 -- Create a user for the ADF pipeline
-CREATE USER [<your_adf_pipeline_name>] FROM EXTERNAL PROVIDER;
+CREATE USER [adf-abnb-cdc-dev] FROM EXTERNAL PROVIDER;
 
-EXEC sp_addrolemember 'db_datareader', [<your_adf_pipeline_name>];
-EXEC sp_addrolemember 'db_datawriter', [<your_adf_pipeline_name>];
-EXEC sp_addrolemember 'db_owner', '<your_adf_pipeline_name>';
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'GDSLearn@123!';
+EXEC sp_addrolemember 'db_datareader', [adf-abnb-cdc-dev];
+EXEC sp_addrolemember 'db_datawriter', [adf-abnb-cdc-dev];
+EXEC sp_addrolemember 'db_owner', 'adf-abnb-cdc-dev';
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'abnbcdcdev@123!';
